4c1f8cb3de45f07d38f3b1bf29db6b95969f8579
JyotiP123 <jyoti.pardhi05@gmail.com>
2022-11-06 18:09:19 +0530

    changes

---
+++ index.js
@@ -0,0 +1,40 @@
+let showgithub = () => {
+    window.open("https://github.com/JyotiP123", "_blank");
+}
+let showresume = () => {
+    window.open("https://drive.google.com/file/d/10lY_7N0o30qTEa2__tpLtO_X8WrLIkWG/view", "_blank");
+}
+let showlinkedin = () => {
+    window.open("https://www.linkedin.com/in/jyoti-pardhi-5b783b14b", "_blank");
+}
+let showmail = () => {
+    window.open("https://mail.google.com/mail/u/0/", "_blank");
+}
+let showinsta = () => {
+    window.open("https://www.instagram.com/jyoti__pardhi/", "_blank");
+}
+
+
+let overstock_git = () => {
+    window.open("https://github.com/vishqaz/Overstock", "_blank");
+}
+let overstock_site = () => {
+    window.open("https://mellifluous-licorice-2028e5.netlify.app/", "_blank");
+}
+let myhour_git = () => {
+    window.open("https://github.com/sushank678/mutual-cart-3380", "_blank");
+}
+let myhour_site = () => {
+    window.open("https://silver-sfogliatella-0c9664.netlify.app/", "_blank");
+}
+let redbus_git = () => {
+    window.open("https://github.com/Abhay0123/ripe-theory-3388", "_blank");
+}
+let redbus_site = () => {
+    window.open("https://phenomenal-kleicha-acfce4.netlify.app/", "_blank");
+}
+
+
+
+GitHubCalendar(".calendar", "JyotiP123");
+GitHubCalendar(".calendar", "JyotiP123", { responsive: true });
